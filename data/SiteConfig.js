a1fc54ccb3f1e9d5a6a17f133019e3179b3268b3
ngraton <mail@ngraton.dev>
2020-07-24 20:27:35 -0400

    update config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,15 +1,15 @@
 const config = {
-  siteTitle: "Nic Graton", // Site title.
-  siteTitleShort: "Nic Graton", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "Shit Nic Says", // Alternative site title for SEO.
+  siteTitle: "ngraton.dev", // Site title.
+  siteTitleShort: "ngraton.dev", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "... and the quest for digital craftsmanship", // Alternative site title for SEO.
   siteLogo: "/logos/flux_capacitor.svg", // Logo used for SEO and manifest.
-  siteUrl: "https://gatsby-advanced-starter-demo.netlify.com", // Domain of your website without pathPrefix.
+  siteUrl: "https://ngraton.dev", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "Shit Nic Says.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "My grail-diary in the pursuit of digital craftsmanship", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteRssTitle: "Nic Graton", // Title of the RSS feed
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
-  dateFormat: "DD/MM/YYYY", // Date format for display.
+  dateFormat: "DD MMM YYYY", // Date format for display.
   userName: "Nic Graton", // Username to display in the author segment.
   userEmail: "mail@ngraton.dev", // Email used for RSS feed's author segment
   userLocation: "Cleveland, Ohio", // User location to display in the author segment.
@@ -25,13 +25,13 @@ const config = {
     },
     {
       label: "Email",
-      url: "mailto:vagr9k@gmail.com",
+      url: "mailto:mail@ngraton.dev",
       iconClassName: "fa fa-envelope"
     }
   ],
   copyright: "Copyright © 2020. Nic Graton", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+  backgroundColor: "#cb11e8" // Used for setting manifest background color.
 };
 
 module.exports = config;
