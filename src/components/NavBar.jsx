629e6483bf300b0d4efe64c0d5d3f3e53369ea10
ngraton <mail@ngraton.dev>
2020-07-26 00:46:55 -0400

    spread out navbar links without losing dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,27 +5,31 @@ function NavBar() {
   const [responsive, setResponsive] = React.useState('')
   return (
     <div className={responsive ? "navbar responsive" : "navbar"}>
-      <div className="navbar-items">
-        <Link className="brand" to="/" key="home">
-          <img className="brand-icon" src="/logos/flux_capacitor.svg" alt="flux capacitor icon" />
-          <span className="brand-text">ngraton.dev</span>
-          <p className="brand-tagline">...and the quest for digital craftsmanship</p>
-        </Link>
-        <Link className="nav-link" to="/writing" key="writing">
-          Writing
-        </Link>
-        <Link className="nav-link" to="/projects" key="projects">
-          Projects
-        </Link>
-        <Link className="nav-link" to="/about" key="about">
-          About
-        </Link>
-        <Link className="nav-link" to="/now" key="now">
-          Now
-        </Link>
-        <button type="button" className="icon" onClick={() => setResponsive(!responsive)}>
-          <i className="fa fa-bars" />
-        </button>
+      <div className="navbar-container">
+        <div className="brand-container">
+          <Link className="brand" to="/" key="home">
+            <img className="brand-icon" src="/logos/flux_capacitor.svg" alt="flux capacitor icon" />
+            <span className="brand-text">ngraton.dev</span>
+            <p className="brand-tagline">...and the quest for digital craftsmanship</p>
+          </Link>
+        </div>
+        <div className="navbar-items">
+          <Link className="nav-link" to="/writing" key="writing">
+            Writing
+          </Link>
+          <Link className="nav-link" to="/projects" key="projects">
+            Projects
+          </Link>
+          <Link className="nav-link" to="/about" key="about">
+            About
+          </Link>
+          <Link className="nav-link" to="/now" key="now">
+            Now
+          </Link>
+          <button type="button" className="icon" onClick={() => setResponsive(!responsive)}>
+            =
+          </button>
+        </div>
       </div>
     </div>
   )
